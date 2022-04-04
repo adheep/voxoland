283e95029d19121d99a25f56e316c385811cc258
Munish <munishwaran.m@myautoiq.com>
2022-04-05 02:56:46 +0530

    add signin

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import logo from './logo.svg';
+import './App.css';
+import { Routes, Route, Outlet, Link } from "react-router-dom";
+import Layout from "./Layout/Layout"
+import Login from './Pages/Login';
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="signin" element={<Login />} />
+          <Route path="dashboard" element={<Dashboard />} />
+
+          {/* Using path="*"" means "match anything", so this route
+                acts like a catch-all for URLs that we don't have explicit
+                routes for. */}
+          {/* <Route path="*" element={<NoMatch />} /> */}
+        </Route>
+      </Routes>
+      
+    </div>
+  );
+}
+
+
+export default App;
+
+
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+    </div>
+  );
+}
---
+++ src/app-config.js
@@ -0,0 +1,5 @@
+const CONFIG = {
+  app: 'VOXOLAND',
+};
+
+module.exports = CONFIG;
